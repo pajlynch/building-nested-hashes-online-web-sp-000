1a25409a308481bda9e547ec651f5400b765bede
Patrick Lynch <pajlynch@gmail.com>
2021-04-28 17:54:00 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -28,11 +28,28 @@ def fourth_challenge
       ]
    },
    :capulet => {
-      :patriarch => {name: "Lord Capulet", age: "50"},
-      :matriarch => {name: "Lady Capulet", age: "51"},
-      :heroine => {name: "Juliet", age: "15", status: "alive"},
+      :patriarch => {
+        name: "Lord Capulet",
+        age: "50"
+      },
+      :matriarch => {
+        name: "Lady Capulet", 
+        age: "51"
+      },
+      :heroine => {name: "Juliet", 
+        age: "15", 
+        status: "alive"
+      },
       :heroine_friends => [
-        #your hashes here!
+        {
+          name: "Steven", 
+          age: "30", 
+          attitude: "confused"
+        },
+        {name: "Nurse", 
+          age: "44", 
+          attitude: "worried"
+        }
       ]
    }
 }
