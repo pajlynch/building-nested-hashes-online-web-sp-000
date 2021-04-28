4938731bbafcc763b5accc546cb9ff8a85dbbf77
Patrick Lynch <pajlynch@gmail.com>
2021-04-28 18:01:21 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -21,8 +21,7 @@ def bonus
   }
 
   epic_tragedy[:montague][:hero][:status] = "dead"
-
-  epic_tragedy[:capulet][:heroine][:status] = "dead"
+  epic_tragedy[:capulet][:heroine][:status] = "deadl"
 
   return epic_tragedy
 end
